ed7403605e1cafcb756974e5028fe3a17c9c9f66
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2022-07-08 20:36:55 +0530

    add router and signin page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './conponents/signin/signin.component';
+import { NavbarComponent } from './conponents/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
