41d96cfa21671199f972956f12cc53b6f43b32d3
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2022-07-08 21:59:13 +0530

    add user details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './conponents/signin/signin.component';
 import { SignupComponent } from './conponents/signup/signup.component';
+import { UserComponent } from './conponents/user/user.component';
+import { UserdetailsComponent } from './conponents/user/userdetails/userdetails.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'signin', pathMatch:'full'},
   {path:'signin', component : SigninComponent},
-  {path: 'signup', component : SignupComponent}
+  {path: 'signup', component : SignupComponent},
+  {path: 'users', component: UserComponent},
+  {path: 'users/userdetails/:id', component: UserdetailsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { SigninComponent } from './conponents/signin/signin.component';
 import { NavbarComponent } from './conponents/navbar/navbar.component';
 import { SignupComponent } from './conponents/signup/signup.component';
+import { UserComponent } from './conponents/user/user.component';
+import { UserlistComponent } from './conponents/user/userlist/userlist.component';
+import { UserdetailsComponent } from './conponents/user/userdetails/userdetails.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
     NavbarComponent,
-    SignupComponent
+    SignupComponent,
+    UserComponent,
+    UserlistComponent,
+    UserdetailsComponent
   ],
   imports: [
     BrowserModule,
