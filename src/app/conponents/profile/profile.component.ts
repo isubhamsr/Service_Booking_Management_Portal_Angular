1f1b6eae8df5e2607b965681827819d38a4e63fa
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2022-07-10 02:39:07 +0530

    update password

--- src/app/conponents/profile/profile.component.ts
+++ src/app/conponents/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute, CanActivate, Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  Password!: string
+
+  url = 'https://localhost:7002/api/User/ChangePassword';
+  error = false;
+  message!: string;
+  isSubmit = false;
+  status!: null;
+  success = false
+
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    console.log('ok');
+    this.isSubmit = true;
+    const data = {
+      Password: this.Password
+    };
+    console.log(data);
+
+    const credentials = JSON.stringify(data);
+    this.http
+      .put(this.url, credentials, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        }),
+      })
+      .subscribe(
+        (response) => {
+          const error = (<any>response).error;
+          if (error) {
+            this.error = true;
+            this.isSubmit = false;
+            this.message = (<any>response).message;
+          } else {
+            this.error = false;
+            // this.toastr.success("Logged In successfully");
+            this.isSubmit = false;
+            this.success = true;
+            this.message = (<any>response).message
+            
+          }
+        },
+        (err) => {
+          this.error = true;
+          this.isSubmit = false;
+          this.message = err.message;
+        }
+      );
+  }
 }
