5389bbd3a81d5787ad8c4a2bdfb7e67c033c0402
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2022-07-10 05:04:38 +0530

    add all report page api

--- src/app/conponents/service/servicereport/servicereporlist/servicereporlist.component.ts
+++ src/app/conponents/service/servicereport/servicereporlist/servicereporlist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Report } from '../../Model/Report'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CanActivate, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servicereporlist',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicereporlistComponent implements OnInit {
 
-  constructor() { }
+  reports!: Report[];
+  url = 'https://localhost:7096/api/ServiceReq/Report';
+  error = false;
+  message!: string;
+  isSubmit = false;
+  status!: null;
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.getAllServiceReports()
+  }
+
+  getAllServiceReports() {
+    this.isSubmit = true;
+    this.http
+      .get(this.url, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        }),
+      })
+      .subscribe(
+        (response) => {
+          const error = (<any>response).error;
+          if (error) {
+            this.error = true;
+            this.isSubmit = false;
+            this.message = (<any>response).message;
+            
+          } else {
+            this.error = false;
+            // this.toastr.success("Logged In successfully");
+            this.isSubmit = false;
+            this.reports = (<any>response).data;
+            
+          }
+        },
+        (err) => {
+          this.isSubmit = false;
+          this.message = "Session Expired, You need to login";
+          this.status = err.status
+        }
+      );    
   }
 
 }
