cb00ae2062441715235a8c7cdde98a7992349f4c
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2022-07-10 02:01:21 +0530

    add product api

--- src/app/conponents/products/addproduct/addproduct.component.ts
+++ src/app/conponents/products/addproduct/addproduct.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute, CanActivate, Router } from '@angular/router';
+import { Product } from '../Model/Product'
 
 @Component({
   selector: 'app-addproduct',
@@ -7,9 +10,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddproductComponent implements OnInit {
 
-  constructor() { }
+  url = 'https://localhost:7121/api/Product';
+  error = false;
+  message!: string;
+  isSubmit = false;
+  status!: null;
+
+  Name!: string
+    Make!: string
+    Model!: number
+    Cost!: number
+
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log('ok');
+    this.isSubmit = true;
+    const product = {
+      Name: this.Name,
+      Make:  this.Make,
+      Model:  this.Model,
+      Cost:  this.Cost,
+    };
+    console.log(product);
+
+    const credentials = JSON.stringify(product);
+    this.http
+      .post(this.url, credentials, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        }),
+      })
+      .subscribe(
+        (response) => {
+          const error = (<any>response).error;
+          if (error) {
+            this.error = true;
+            this.isSubmit = false;
+            this.message = (<any>response).message;
+          } else {
+            this.error = false;
+            // this.toastr.success("Logged In successfully");
+            this.isSubmit = false;
+            console.log((<any>response).message);
+            
+            this.router.navigate(['/products']);
+          }
+        },
+        (err) => {
+          this.error = true;
+          this.isSubmit = false;
+          this.message = err.message;
+        }
+      );
   }
 
 }
