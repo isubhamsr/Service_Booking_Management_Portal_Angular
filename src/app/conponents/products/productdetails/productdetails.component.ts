a201ca6bd2d2e81f3304d515199ec8e1268fafcd
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2022-07-10 01:44:53 +0530

    add product details api

--- src/app/conponents/products/productdetails/productdetails.component.ts
+++ src/app/conponents/products/productdetails/productdetails.component.ts
@@ -10,9 +10,61 @@ import {formatDate } from '@angular/common';
 })
 export class ProductdetailsComponent implements OnInit {
 
-  constructor() { }
+  url = 'https://localhost:7121/api/Product';
+  error = false;
+  message!: string;
+  isSubmit = false;
+  status!: null;
+
+  Id!: number
+    Name!: string
+    Make!: string
+    Model!: number
+    Cost!: number
+    CreatedDate!: string
+
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    let id = this.activatedRoute.snapshot.params['id'];
+    this.getProduct(id)
+  }
+
+  getProduct(id: string) {
+    this.isSubmit = true;
+    this.http
+      .get(`${this.url}/${id}`, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        }),
+      })
+      .subscribe(
+        (response) => {
+          const error = (<any>response).error;
+          if (error) {
+            this.error = true;
+            this.isSubmit = false;
+            this.message = (<any>response).message;
+            
+          } else {
+            this.error = false;
+            // this.toastr.success("Logged In successfully");
+            this.isSubmit = false;
+            
+            this.Name = (<any>response).data[0].Name
+            this.Make = (<any>response).data[0].Make
+            this.Model = (<any>response).data[0].Model
+            this.Cost = (<any>response).data[0].Cost
+            this.CreatedDate = formatDate((<any>response).data[0].CreatedDate, 'yyyy-MM-dd', 'en-US');
+          }
+        },
+        (err) => {
+          this.isSubmit = false;
+          this.message = "Session Expired, You need to login";
+          this.status = err.status
+        }
+      );
   }
 
 }
