d668f22786fb19d49efd36405d19e3300cc9de05
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2022-07-11 14:29:44 +0530

    add product id in add report section

--- src/app/conponents/service/servicereport/addservicereporrt/addservicereporrt.component.ts
+++ src/app/conponents/service/servicereport/addservicereporrt/addservicereporrt.component.ts
@@ -10,6 +10,7 @@ import { ActivatedRoute, CanActivate, Router } from '@angular/router';
 export class AddservicereporrtComponent implements OnInit {
 
   url = 'https://localhost:7096/api/ServiceReq/Report';
+  booking = 'https://localhost:7096/api/ServiceReq';
   error = false;
   message!: string;
   isSubmit = false;
@@ -23,12 +24,22 @@ export class AddservicereporrtComponent implements OnInit {
     IsPaid!: string
     VisitFees!: number
     RepairDetails!: string
-    SerReqId = 5
+    SerReqId!: number
     ServiceType!: string
 
   constructor(private activatedRoute: ActivatedRoute, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    let token = localStorage.getItem('token')
+    if(token !== null){
+      let jwt = JSON.parse(atob(token.split('.')[1]))
+      let id = parseInt(jwt.Id)
+      this.getAllServiceReqs(id)
+    }
+    else{
+      this.error = true
+      this.message = "Token not found"
+    }
   }
 
   onSubmit(){
@@ -76,6 +87,40 @@ export class AddservicereporrtComponent implements OnInit {
           this.message = err.message;
         }
       );
+  }
+
+  getAllServiceReqs(id:number) {
+    this.isSubmit = true;
+    this.http
+      .get(`${this.booking}/${id}`, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        }),
+      })
+      .subscribe(
+        (response) => {
+          const error = (<any>response).error;
+          if (error) {
+            this.error = true;
+            this.isSubmit = false;
+            this.message = (<any>response).message;
+            
+          } else {
+            this.error = false;
+            // this.toastr.success("Logged In successfully");
+            this.isSubmit = false;
+            this.SerReqId = (<any>response).data[0].Id;
+            console.log(this.SerReqId);
+            
+          }
+        },
+        (err) => {
+          this.isSubmit = false;
+          this.message = "Session Expired, You need to login";
+          this.status = err.status
+        }
+      );    
   }
 
 }
